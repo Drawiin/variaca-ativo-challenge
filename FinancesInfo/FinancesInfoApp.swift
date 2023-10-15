bd298ba85f2139e3a37b0402bb0b9d23ab18b1ea
drawiin <vini65599@gmail.com>
2023-10-15 13:42:52 -0300

    Initial Commit

---
+++ FinancesInfo/FinancesInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancesInfoApp.swift
+//  FinancesInfo
+//
+//  Created by Vinicius Guimarães on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FinancesInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
